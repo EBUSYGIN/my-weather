4629035a9bd1746c19ddefc8811d2d6fc6c1b056
Egor <e.busygin2003@gmail.com>
2025-07-11 10:55:23 +0300

    add input, and create header

---
+++ src/ui/1-widgets/Header/Header.tsx
@@ -0,0 +1,24 @@
+import { Icon, Input } from '@/ui/4-shared';
+
+import styles from './Header.module.css';
+
+export function Header() {
+  const dateFormatOptions: Intl.DateTimeFormatOptions = {
+    weekday: 'short',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  return (
+    <div className={styles.header}>
+      <Icon.Logo />
+      <Input icon='Magnifier' placeholder='Введите город...' />
+      <div className={styles.date}>
+        {new Intl.DateTimeFormat('ru-RU', dateFormatOptions).format(new Date())}
+      </div>
+      <p className={styles.tuneText}>
+        Будущее кажется светлым — оставайтесь с нами!
+      </p>
+    </div>
+  );
+}
---
+++ src/ui/4-shared/Icon/Icon.tsx
@@ -0,0 +1,9 @@
+import Logo from '@assets/images/icons/logo.svg';
+import Magnifier from '@assets/images/icons/magnifier.svg';
+
+export const Icon = {
+  Logo,
+  Magnifier,
+};
+
+export type Icon = keyof typeof Icon;
