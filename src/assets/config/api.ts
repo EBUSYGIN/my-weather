9813e8ccc45b067de71834dfc4e56fb937e5efd3
Egor Busygin <e.busygin2003@gmail.com>
2025-10-15 09:30:53 +0300

    fixes

--- src/assets/config/api.ts
+++ src/assets/config/api.ts
@@ -1,3 +1,3 @@
 export const API_URL = process.env.WEATHER_API;
 export const GEOLOCATION_API_URL = process.env.GEOLOCATION_API;
-export const GEOLOCATION_KEY = process.env.DA_DATA_API_KEY;
+export const GEOLOCATION_KEY = process.env.GEOLOCATION_KEY;
